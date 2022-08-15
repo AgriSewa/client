62a051f35e97bd3be9a3290eac8f2e33b5202b8d
agnivg <agnivg157@gmail.com>
2022-08-16 01:28:20 +0530

    changes to Results date handling

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const Results=()=>{
     const navigate = useNavigate();
     const [results,setResults]=useState([]);
+    const curdate=new Date();
     useEffect(()=>{
         if (!localStorage.getItem("user")) {
             navigate("/api/auth/login");
@@ -30,6 +31,7 @@ const Results=()=>{
                     var date=new Date(result.book_date.split('T')[0])
                     date.setDate(date.getDate()+1)
                     return(
+                        date.toDateString()<=curdate.toDateString() && 
                         <div className='card home-card' key={result.id}>                     
                             <div className='card-image'>
                                 <img src={result.image} style={{height:'300px'}}/>
